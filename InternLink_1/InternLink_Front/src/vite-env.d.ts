629af5ba42c5663aaf80d7b72b4a11211cea00cf
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-09-30 20:54:48 +0530

    The ClassOverriding Issue Done

---
+++ InternLink_1/InternLink_Front/src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+// Let TS know it's valid to import CSS files
+declare module '*.css';
+
+// (Optional) if you import images/SVGs directly
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
