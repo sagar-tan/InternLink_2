e240d1189d85a5df4b2e0c390f4d2ab1d54e9805
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-10-02 09:54:03 +0530

    Spring_Init_Setup

---
+++ InternLink_1/backend/src/main/java/com/internlink/backend/InternlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internlink.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternlinkBackendApplication.class, args);
+	}
+
+}
---
+++ InternLink_1/backend/src/test/java/com/internlink/backend/InternlinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internlink.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternlinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
