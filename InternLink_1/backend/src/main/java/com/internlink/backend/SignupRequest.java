2678b86fb03bd2d792e4e540fd06898e79643c5b
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-10-02 10:49:21 +0530

    SignupRequest_1

---
+++ InternLink_1/backend/src/main/java/com/internlink/backend/SignupRequest.java
@@ -0,0 +1,18 @@
+package com.internlink.backend;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class SignupRequest {
+    private String fullName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String organization;
+    private String phone;
+    private String role;
+    private String userType; // "student" or "employer"
+    //we will have to add a Captcha field here later
+}
