dbf35bf8f86f8f9128d4470b8a036b0c2fcb248a
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-10-09 23:01:53 +0530

    UserRepository Setup

--- InternLink_1/backend/src/main/java/com/internlink/users/repository/UserRepository.java
+++ InternLink_1/backend/src/main/java/com/internlink/users/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package com.internlink.users.repository;
 
-public class UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.internlink.users.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
     
 }
+
+//this file is used to interact with the database for User entity
