dfc9972d752be73d543ea2618aae1013834e1f91
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-10-09 23:26:04 +0530

    Auth Controller adn JWT Config Done

--- InternLink_1/backend/src/main/java/com/internlink/auth/controller/AuthController.java
+++ InternLink_1/backend/src/main/java/com/internlink/auth/controller/AuthController.java
@@ -1,5 +1,26 @@
 package com.internlink.auth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.internlink.auth.model.AuthRequest;
+import com.internlink.auth.model.AuthResponse;
+import com.internlink.auth.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
-    
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        return authService.login(request);
+    }
 }
