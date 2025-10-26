ac7e1a8f8cd0d38b308daf9d23eb229c01790ffb
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-10-26 12:05:04 +0530

    CPR_Dto Done

--- InternLink_1/backend/src/main/java/com/internlink/backend/dto/CandidateProfileRequest.java
+++ InternLink_1/backend/src/main/java/com/internlink/backend/dto/CandidateProfileRequest.java
@@ -1,6 +1,32 @@
 package com.internlink.backend.dto;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class CandidateProfileRequest {
-    
-    
+    private Long userId; // Linked user
+    private String gender;
+    private String dateOfBirth;
+    private String citizenship;
+    private Boolean currentlyEmployed;
+    private String currentAddress;
+    private String city;
+    private String state;
+    private String pincode;
+    private String category;
+    private Boolean pwd;
+    private String pwdType;
+    private Boolean fgg;
+    private Boolean govtEmployee;
+    private String govtEmployeeDetails;
+    private String familyIncome;
+    private String fatherOccupation;
+    private String motherOccupation;
+//to be added
+    private List<EducationDTO> education;
+    private List<ExperienceDTO> experience;
+    private List<String> skills;
+    private PreferencesDTO preferences;
+    private ParticipationDTO participation;
 }
