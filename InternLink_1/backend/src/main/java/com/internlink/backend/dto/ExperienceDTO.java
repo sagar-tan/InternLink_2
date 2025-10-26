c51870faaaa4eac29473cbd1fba7d65b764ac475
Sagar <56674688+sagar-tan@users.noreply.github.com>
2025-10-26 12:08:20 +0530

    Adding Preferences DTO

--- InternLink_1/backend/src/main/java/com/internlink/backend/dto/ExperienceDTO.java
+++ InternLink_1/backend/src/main/java/com/internlink/backend/dto/ExperienceDTO.java
@@ -1,5 +1,14 @@
 package com.internlink.backend.dto;
 
+import lombok.Data;
+
+@Data
 public class ExperienceDTO {
-    
+    private String companyName;
+    private String position;
+    private String startDate;
+    private String endDate;
+    private String responsibilities;
+    private String keyAchievements;
+    private Boolean workHereNow;
 }
